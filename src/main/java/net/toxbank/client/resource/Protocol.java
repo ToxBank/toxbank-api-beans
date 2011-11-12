6a5d7ceab7b68f59606d4fbee0c1fcf0947c494d
Egon Willighagen <egon.willighagen@gmail.com>
2011-11-12 11:49:41 +0100

    First API method made visible at a client API level

--- src/main/java/net/toxbank/client/resource/Protocol.java
+++ src/main/java/net/toxbank/client/resource/Protocol.java
@@ -1,5 +1,16 @@
 package net.toxbank.client.resource;
 
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+
 public class Protocol {
 
+	/**
+	 * Described in this <a href="http://api.toxbank.net/index.php/API_Protocol:RetrieveList">API documentation</a>.
+	 */
+	public static List<URL> listProtocols(String server) {
+		return Collections.emptyList();
+	}
+	
 }
--- src/test/java/net/toxbank/client/resource/ProtocolTest.java
+++ src/test/java/net/toxbank/client/resource/ProtocolTest.java
@@ -1,14 +1,26 @@
 package net.toxbank.client.resource;
 
+import java.net.URL;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ProtocolTest {
-	
+
+	private final static String TEST_SERVER = "http://demo.toxbank.net/";
+
 	@Test
 	public void testConstructor() {
 		Protocol clazz = new Protocol();
 		Assert.assertNotNull(clazz);
 	}
 
+	@Test
+	public void testListProtocols() {
+		List<URL> protocols = Protocol.listProtocols(TEST_SERVER);
+		Assert.assertNotNull(protocols);
+		Assert.assertNotSame(0, protocols.size());
+	}
+	
 }
