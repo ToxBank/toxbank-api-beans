de8962e74802d2a4a11735da66a3b162b973653d
Egon Willighagen <egon.willighagen@gmail.com>
2011-11-12 11:10:20 +0100

    Added inital class

---
+++ src/test/java/net/toxbank/client/resource/AssayTest.java
@@ -0,0 +1,14 @@
+package net.toxbank.client.resource;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AssayTest {
+	
+	@Test
+	public void testConstructor() {
+		Assay assay = new Assay();
+		Assert.assertNotNull(assay);
+	}
+
+}
