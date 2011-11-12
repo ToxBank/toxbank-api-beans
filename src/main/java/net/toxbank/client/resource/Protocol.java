8f31c2ce8e020ed6ae316c024ab320b315a5272c
Egon Willighagen <egon.willighagen@gmail.com>
2011-11-12 12:02:07 +0100

    Added more testing for the Protocol API

--- src/main/java/net/toxbank/client/resource/Protocol.java
+++ src/main/java/net/toxbank/client/resource/Protocol.java
@@ -6,11 +6,35 @@ import java.util.List;
 
 public class Protocol {
 
+	private URL organisation;
+
+	public Protocol() {}
+	
+	/**
+	 * Described in this <a href="http://api.toxbank.net/index.php/API_Protocol:RetrieveMetadata">API documentation</a>.
+	 */
+	public Protocol(URL identifier) {
+		// FIXME: implement retrieving metadata from the URL and set the below fields
+		this.organisation = null;
+	}
+	
 	/**
 	 * Described in this <a href="http://api.toxbank.net/index.php/API_Protocol:RetrieveList">API documentation</a>.
 	 */
 	public static List<URL> listProtocols(String server) {
+		// FIXME: retrieve a list of all registered protocols
 		return Collections.emptyList();
 	}
-	
+
+	/**
+	 * Described in this <a href="http://api.toxbank.net/index.php/API_Protocol:Uploadt">API documentation</a>.
+	 */
+	public URL upload(String server) {
+		// FIXME: implement uploading this protocol to the server
+		return null;
+	}
+
+	public URL getOrganisation() {
+		return this.organisation;
+	}
 }
--- src/test/java/net/toxbank/client/resource/ProtocolTest.java
+++ src/test/java/net/toxbank/client/resource/ProtocolTest.java
@@ -1,5 +1,6 @@
 package net.toxbank.client.resource;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
@@ -21,6 +22,24 @@ public class ProtocolTest {
 		List<URL> protocols = Protocol.listProtocols(TEST_SERVER);
 		Assert.assertNotNull(protocols);
 		Assert.assertNotSame(0, protocols.size());
+	}
+	
+	@Test
+	public void testUpload() {
+		Protocol protocol = new Protocol();
+		URL url = protocol.upload(TEST_SERVER);
+		Assert.assertNotNull(url);
+		Assert.assertTrue(url.toExternalForm().startsWith(TEST_SERVER));
+	}
+	
+	@Test
+	public void testRetrieveMetadata() throws MalformedURLException {
+		Protocol protocol = new Protocol(new URL(TEST_SERVER + "protocol/1"));
+		Assert.assertNotNull(protocol);
+		Assert.assertEquals(
+			TEST_SERVER + "organization/1",
+			protocol.getOrganisation()
+		);
 	}
 	
 }
