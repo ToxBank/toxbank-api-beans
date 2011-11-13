d7cdab1907b06736cdf51d57a8ce37db8e06166a
Egon Willighagen <egon.willighagen@gmail.com>
2011-11-13 09:53:28 +0100

    More API mockup: Study

--- src/main/java/net/toxbank/client/resource/Study.java
+++ src/main/java/net/toxbank/client/resource/Study.java
@@ -1,5 +1,127 @@
 package net.toxbank.client.resource;
 
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+
 public class Study {
 
+	private List<String> keywords;
+	private String abstrakt;
+	private boolean isSummarySearchable;
+	private User author;
+	private Assay assay;
+	private Organisation owner;
+	private Project project;
+	private String versionInfo;
+
+	public Study() {}
+	
+	/**
+	 * Described in this <a href="http://api.toxbank.net/index.php/API_Study:Retrieve">API documentation</a>.
+	 */
+	public Study(URL identifier) {
+		// FIXME: implement retrieving metadata from the URL and set the below fields
+		this.setOwner(null);
+	}
+
+	public void setOwner(Organisation owner) {
+		this.owner = owner;
+	}
+
+	public Organisation getOwner() {
+		return owner;
+	}
+
+	/**
+	 * Described in this <a href="http://api.toxbank.net/index.php/API_Study:RetrieveList">API documentation</a>.
+	 */
+	public static List<URL> list(String server) {
+		// FIXME: retrieve a list of all registered protocols
+		return Collections.emptyList();
+	}
+
+	/**
+	 * Described in this <a href="http://api.toxbank.net/index.php/API_Study:Upload">API documentation</a>.
+	 */
+	public URL upload(String server) {
+		// FIXME: implement uploading this protocol to the server
+		return null;
+	}
+
+	public void addKeyword(String keyword) {
+	}
+
+	public void removeKeyword(String keyword) {
+	}
+
+	public List<String> getKeywords() {
+		return keywords;
+	}
+
+	public void setAbstract(String abstrakt) {
+		this.abstrakt = abstrakt;
+	}
+
+	public String getAbstract() {
+		return abstrakt;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setSummarySearchable(boolean isSummarySearchable) {
+		this.isSummarySearchable = isSummarySearchable;
+	}
+
+	public boolean isSummarySearchable() {
+		return isSummarySearchable;
+	}
+
+	public void setAssay(Assay assay) {
+		this.assay = assay;
+	}
+
+	public Assay getAssay() {
+		return assay;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setVersionInfo(String versionInfo) {
+		this.versionInfo = versionInfo;
+	}
+
+	public String getVersionInfo() {
+		return versionInfo;
+	}
+
+	/**
+	 * Described in this <a href="http://api.toxbank.net/index.php/API_Study:RetrieveVersions">API documentation</a>.
+	 */
+	public List<URL> listVersions() {
+		// FIXME: implement uploading this protocol to the server
+		return null;
+	}
+
+	/**
+	 * Described in this <a href="http://api.toxbank.net/index.php/API_Study:RetrieveVersions">API documentation</a>.
+	 * Equivalent to {@link #listVersions()} but returns {@link Study}s
+	 * already populated with metadata from the database.
+	 */
+	public List<Study> getVersions() {
+		// FIXME: implement uploading this protocol to the server
+		return null;
+	}
 }
--- src/test/java/net/toxbank/client/resource/StudyTest.java
+++ src/test/java/net/toxbank/client/resource/StudyTest.java
@@ -1,14 +1,91 @@
 package net.toxbank.client.resource;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class StudyTest {
 	
+	private final static String TEST_SERVER = "http://demo.toxbank.net/";
+
 	@Test
 	public void testConstructor() {
 		Study clazz = new Study();
 		Assert.assertNotNull(clazz);
 	}
 
+	@Test
+	public void testRetrieveMetadata() throws MalformedURLException {
+		Study study = new Study(new URL(TEST_SERVER + "protocol/1"));
+		Assert.assertNotNull(study);
+		Assert.assertEquals(
+			TEST_SERVER + "organization/1",
+			study.getOwner()
+		);
+	}
+
+	@Test
+	public void testList() {
+		List<URL> studies = Study.list(TEST_SERVER);
+		Assert.assertNotNull(studies);
+		Assert.assertNotSame(0, studies.size());
+	}
+
+	@Test
+	public void testUpload() {
+		Study study = new Study();
+		URL url = study.upload(TEST_SERVER);
+		Assert.assertNotNull(url);
+		Assert.assertTrue(url.toExternalForm().startsWith(TEST_SERVER));
+	}
+
+	@Test
+	public void testListVersions() throws MalformedURLException {
+		Study study = new Study(new URL(TEST_SERVER + "protocol/1"));
+		List<URL> versions = study.listVersions();
+		Assert.assertNotNull(versions);
+		Assert.assertNotSame(0, versions.size());
+	}
+	
+	@Test
+	public void testGetVersions() throws MalformedURLException {
+		Study study = new Study(new URL(TEST_SERVER + "protocol/1"));
+		List<Study> versions = study.getVersions();
+		Assert.assertNotNull(versions);
+		Assert.assertNotSame(0, versions.size());
+		Assert.assertTrue(versions.contains(study));
+	}
+
+	@Test
+	public void testGetSetKeywords() {
+		Study study = new Study();
+		Assert.assertEquals(0, study.getKeywords().size());
+		study.addKeyword("foo");
+		Assert.assertEquals(1, study.getKeywords().size());
+		Assert.assertTrue(study.getKeywords().contains("foo"));
+		study.removeKeyword("foo");
+		Assert.assertEquals(0, study.getKeywords().size());
+		Assert.assertFalse(study.getKeywords().contains("foo"));
+	}
+
+	@Test
+	public void testGetSetAbstract() {
+		Study study = new Study();
+		Assert.assertNull(study.getAbstract());
+		study.setAbstract("This is an abstract");
+		Assert.assertNotNull(study.getAbstract());
+		Assert.assertEquals(19, study.getAbstract().length());
+	}
+
+	@Test
+	public void testGetSetVersionInfo() {
+		Study study = new Study();
+		Assert.assertNull(study.getVersionInfo());
+		study.setVersionInfo("1");
+		Assert.assertNotNull(study.getVersionInfo());
+		Assert.assertEquals("1", study.getVersionInfo());
+	}
 }
