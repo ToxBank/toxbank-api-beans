16f333031eb82ad14cce04eb4cf8f186e6fb4da0
Egon Willighagen <egon.willighagen@gmail.com>
2011-11-24 15:25:31 +0100

    Added serialization into turtle

--- src/main/java/net/toxbank/client/io/rdf/Serializer.java
+++ src/main/java/net/toxbank/client/io/rdf/Serializer.java
@@ -4,6 +4,7 @@ import java.io.OutputStream;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.vocabulary.DCTerms;
+import com.hp.hpl.jena.vocabulary.XSD;
 
 /**
  * Helper class to create RDF/XML and other serializations.
@@ -15,8 +16,14 @@ public class Serializer {
 	public static void toRDFXML(OutputStream output, Model model) {
 		model.setNsPrefix("tb", TOXBANK.URI);
 		model.setNsPrefix("dcterms", DCTerms.getURI());
+		model.setNsPrefix("xsd", XSD.getURI());
 		model.write(output, "RDF/XML-ABBREV");
 	}
 
-
+	public static void toTurtle(OutputStream output, Model model) {
+		model.setNsPrefix("tb", TOXBANK.URI);
+		model.setNsPrefix("dcterms", DCTerms.getURI());
+		model.setNsPrefix("xsd", XSD.getURI());
+		model.write(output, "TURTLE");
+	}
 }
