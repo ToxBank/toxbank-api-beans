28bc4ff1f8ff511a384b9b61e93eb78750dd767e
Egon Willighagen <egon.willighagen@gmail.com>
2011-11-24 13:13:17 +0100

    Added reading of a Model into a Protocol

--- src/main/java/net/toxbank/client/io/rdf/ProtocolIO.java
+++ src/main/java/net/toxbank/client/io/rdf/ProtocolIO.java
@@ -1,5 +1,8 @@
 package net.toxbank.client.io.rdf;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,6 +10,8 @@ import net.toxbank.client.resource.Protocol;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.ResIterator;
+import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.RDF;
 
 public class ProtocolIO implements IOClass<Protocol> {
@@ -30,7 +35,27 @@ public class ProtocolIO implements IOClass<Protocol> {
 
 	public List<Protocol> fromJena(Model source) {
 		if (source == null) return Collections.emptyList();
-		return (List<Protocol>)null;
+
+		ResIterator iter = source.listResourcesWithProperty(RDF.type, TOXBANK.PROTOCOL);
+		if (!iter.hasNext()) return Collections.emptyList();
+
+		List<Protocol> protocols = new ArrayList<Protocol>();
+		while (iter.hasNext()) {
+			Protocol protocol = new Protocol();
+			Resource res = iter.next();
+			try {
+				protocol.setResourceURL(
+					new URL(res.getURI())
+				);
+			} catch (MalformedURLException e) {
+				throw new IllegalArgumentException(
+					"Found resource with an invalid URI:" + res.getURI()
+				);
+			}
+			protocols.add(protocol);
+		}
+
+		return protocols;
 	}
 
 }
--- src/test/java/net/toxbank/client/io/rdf/ProtocolIOTest.java
+++ src/test/java/net/toxbank/client/io/rdf/ProtocolIOTest.java
@@ -1,11 +1,40 @@
 package net.toxbank.client.io.rdf;
 
+import java.net.URL;
+import java.util.List;
+
+import junit.framework.Assert;
+
 import net.toxbank.client.resource.Protocol;
+
+import org.junit.Test;
+
+import com.hp.hpl.jena.rdf.model.Model;
 
 public class ProtocolIOTest extends AbstractIOClassTest<Protocol> {
 
 	public ProtocolIO getIOClass() {
 		return new ProtocolIO();
+	}
+
+	@Test
+	public void testRoundtripResourceURI() throws Exception {
+		ProtocolIO ioClass = getIOClass();
+		
+		Protocol testProtocol = new Protocol();
+		testProtocol.setResourceURL(new URL("http://example.org/testProtocol/666"));
+		Model model = ioClass.toJena(
+			null, // create a new class
+			testProtocol
+		);
+
+		List<Protocol> roundTrippedProtocols = ioClass.fromJena(model);
+		model.write(System.out, "RDF/XML");
+		Assert.assertEquals(1, roundTrippedProtocols.size());
+		Assert.assertEquals(
+			"http://example.org/testProtocol/666",
+			roundTrippedProtocols.get(0).getResourceURL().toString()
+		);
 	}
 
 }
