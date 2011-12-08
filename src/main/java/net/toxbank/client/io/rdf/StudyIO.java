0411312ee537b5f70451b1669b95cdf0619ff43c
Egon Willighagen <egon.willighagen@gmail.com>
2011-12-08 14:24:23 +0100

    Added more IO for studies

--- src/main/java/net/toxbank/client/io/rdf/StudyIO.java
+++ src/main/java/net/toxbank/client/io/rdf/StudyIO.java
@@ -6,26 +6,45 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import net.toxbank.client.resource.Organisation;
 import net.toxbank.client.resource.Study;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.DCTerms;
 import com.hp.hpl.jena.vocabulary.RDF;
 
 public class StudyIO implements IOClass<Study> {
 
-	public Model toJena(Model toAddTo, Study... accounts) {
+	public Model toJena(Model toAddTo, Study... studies) {
 		if (toAddTo == null) toAddTo = ModelFactory.createDefaultModel();
-		if (accounts == null) return toAddTo;
+		if (studies == null) return toAddTo;
 
-		for (Study study : accounts) {
+		for (Study study : studies) {
 			if (study.getResourceURL() == null) {
 				throw new IllegalArgumentException(String.format(msg_ResourceWithoutURI, "Studies"));
 			}
 			Resource res = toAddTo.createResource(study.getResourceURL().toString());
 			toAddTo.add(res, RDF.type, TOXBANK.STUDY);
+			if (study.getTitle() != null)
+				res.addLiteral(DCTerms.title, study.getTitle());
+			List<String> keywords = study.getKeywords();
+			if (keywords != null) {
+				for (String keyword : keywords)
+					res.addLiteral(TOXBANK.HASKEYWORD, keyword);
+			}
+			if (study.getOwner() != null) {
+				if (study.getOwner().getResourceURL()==null)
+					throw new IllegalArgumentException(String.format(msg_InvalidURI, "study owner",res.getURI()));
+					
+				Resource ownerRes = toAddTo.createResource(
+					study.getOwner().getResourceURL().toString()
+				);
+				res.addProperty(TOXBANK.HASOWNER, ownerRes);
+			}
 		}
 		return toAddTo;
 	}
@@ -36,7 +55,7 @@ public class StudyIO implements IOClass<Study> {
 		ResIterator iter = source.listResourcesWithProperty(RDF.type, TOXBANK.STUDY);
 		if (!iter.hasNext()) return Collections.emptyList();
 
-		List<Study> accounts = new ArrayList<Study>();
+		List<Study> studies = new ArrayList<Study>();
 		while (iter.hasNext()) {
 			Study study = new Study();
 			Resource res = iter.next();
@@ -48,10 +67,30 @@ public class StudyIO implements IOClass<Study> {
 				throw new IllegalArgumentException(String.format(msg_InvalidURI,"an account",res.getURI())
 				);
 			}
-			accounts.add(study);
+			if (res.getProperty(DCTerms.title) != null)
+				study.setTitle(res.getProperty(DCTerms.title).getString());
+			StmtIterator keywords = res.listProperties(TOXBANK.HASKEYWORD);
+			while (keywords.hasNext()) {
+				study.addKeyword(keywords.next().getString());
+			}
+			String uri = null;
+			if (res.getProperty(TOXBANK.HASOWNER) != null)
+				try {
+					uri = res.getProperty(TOXBANK.HASOWNER).getResource().getURI();		
+					Organisation org = new Organisation();
+					
+					org.setResourceURL(
+						new URL(uri)
+					);
+					study.setOwner(org);
+				} catch (MalformedURLException e) {
+					throw new IllegalArgumentException(String.format(msg_InvalidURI,"a study owner",uri));
+				}
+
+			studies.add(study);
 		}
 
-		return accounts;
+		return studies;
 	}
 
 }
--- src/test/java/net/toxbank/client/io/rdf/StudyIOTest.java
+++ src/test/java/net/toxbank/client/io/rdf/StudyIOTest.java
@@ -2,10 +2,12 @@ package net.toxbank.client.io.rdf;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
 import junit.framework.Assert;
+import net.toxbank.client.resource.Organisation;
 import net.toxbank.client.resource.Study;
 
 import org.junit.Test;
@@ -16,6 +18,61 @@ public class StudyIOTest extends AbstractIOClassTest<Study> {
 
 	public StudyIO getIOClass() {
 		return new StudyIO();
+	}
+
+	@Test
+	public void testRoundtripTitle() throws MalformedURLException, IOException {
+		Study study = new Study();
+		study.setResourceURL(new URL("http://example.org/testStudy/666"));
+		study.setTitle("Title");
+
+		Study roundtripped = roundtripSingleStudy(study);
+
+		Assert.assertEquals("Title", roundtripped.getTitle());
+	}
+
+	@Test
+	public void testRoundtripTitle_Null() throws MalformedURLException, IOException {
+		Study study = new Study();
+		study.setResourceURL(new URL("http://example.org/testStudy/666"));
+		study.setTitle(null);
+
+		Study roundtripped = roundtripSingleStudy(study);
+
+		Assert.assertNull(roundtripped.getTitle());
+	}
+
+	@Test
+	public void testRoundtripKeywords() throws MalformedURLException, IOException {
+		Study study = new Study();
+		study.setResourceURL(new URL("http://example.org/testStudy/666"));
+		study.addKeyword("key");
+		study.addKeyword("word");
+
+		Study roundtripped = roundtripSingleStudy(study);
+
+		List<String> keywords = roundtripped.getKeywords();
+		Assert.assertNotNull(keywords);
+		Assert.assertEquals(2, keywords.size());
+		Assert.assertTrue(keywords.contains("key"));
+		Assert.assertTrue(keywords.contains("word"));
+	}
+
+	@Test
+	public void testRoundtripOwner() throws MalformedURLException, IOException {
+		Study study = new Study();
+		study.setResourceURL(new URL("http://example.org/testProtocol/666"));
+		Organisation owner = new Organisation();
+		owner.setResourceURL(new URL("http://example.org/testOrg/B.Bub"));
+		study.setOwner(owner);
+
+		Study roundtripped = roundtripSingleStudy(study);
+
+		Assert.assertNotNull(roundtripped.getOwner());
+		Assert.assertEquals(
+			"http://example.org/testOrg/B.Bub",
+			roundtripped.getOwner().getResourceURL().toString()
+		);
 	}
 
 	@Test
