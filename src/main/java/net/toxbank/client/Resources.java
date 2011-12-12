6853623e6b8c207541e6ec881dbc7296624a6832
ngn <jeliazkova.nina@gmail.com>
2011-12-12 18:31:23 +0200

    Moved resource prefixes from toxbank-server/toxbank-interface module

---
+++ src/main/java/net/toxbank/client/Resources.java
@@ -0,0 +1,13 @@
+package net.toxbank.client;
+
+public class Resources {
+	public static final String datatemplate = "/datatemplate";
+	public static final String protocol = "/protocol";
+	public static final String project = "/project";
+	public static final String organisation = "/organisation";
+	public static final String user = "/user";
+	public static final String versions = "/versions";
+	public static final String authors = "/authors";
+	public static final String document = "/document";
+
+}
