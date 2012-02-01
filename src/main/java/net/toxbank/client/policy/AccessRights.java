d9ce4c4f97d3432b25fd8a676bb596ed8857bb9f
ngn <jeliazkova.nina@gmail.com>
2012-02-01 15:23:18 +0200

    Added more helper methods

--- src/main/java/net/toxbank/client/policy/AccessRights.java
+++ src/main/java/net/toxbank/client/policy/AccessRights.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import net.toxbank.client.resource.Group;
+import net.toxbank.client.resource.User;
+
 /**
  * 
  * @author nina
@@ -29,14 +32,82 @@ public class AccessRights {
 	public void setRules(List<PolicyRule> rules) {
 		this.rules = rules;
 	}
-	
+	/**
+	 * 
+	 * @param userName
+	 * @param get
+	 * @param post
+	 * @param put
+	 * @param delete
+	 * @return
+	 */
+	public PolicyRule addUserRule(String userName,Boolean get, Boolean post, Boolean put, Boolean delete) {
+		User user = new User();
+		user.setUserName(userName);
+		return addUserRule(user,get,post,put,delete);
+	}
+	/**
+	 * 
+	 * @param groupName
+	 * @param get
+	 * @param post
+	 * @param put
+	 * @param delete
+	 * @return
+	 */
+	public PolicyRule addGroupRule(String groupName,Boolean get, Boolean post, Boolean put, Boolean delete) {
+		Group group = new Group();
+		group.setGroupName(groupName);
+		return addGroupRule(group,get,post,put,delete);
+	}
+	/**
+	 * GET only allowed
+	 * @param user  Should have non null username!
+	 * @return
+	 */
+	public PolicyRule addReadOnlyUserRule(User user) {
+		return addUserRule(user,true,false,false,false);
+	}
+	/**
+	 * GET only allowed
+	 * @param group  Should have non null groupname!
+	 * @return
+	 */
+	public PolicyRule addReadOnlyGroupRule(Group group) {
+		return addGroupRule(group,true,false,false,false);
+	}
+	/**
+	 * 
+	 * @param user
+	 * @param get
+	 * @param post
+	 * @param put
+	 * @param delete
+	 * @return
+	 */
+	public PolicyRule addUserRule(User user, Boolean get, Boolean post, Boolean put, Boolean delete) {
+		return addRule(new UserPolicyRule<User>(user,get,post,put,delete));
+	}
+	/**
+	 * 
+	 * @param group
+	 * @param get
+	 * @param post
+	 * @param put
+	 * @param delete
+	 * @return
+	 */
+	public PolicyRule addGroupRule(Group group, Boolean get, Boolean post, Boolean put, Boolean delete) {
+		return addRule(new GroupPolicyRule<Group>(group,get,post,put,delete));
+	}
 	/**
 	 * Add policy rule
 	 * @param rule
 	 */
-	public void addRule(PolicyRule rule) {
+	public PolicyRule addRule(PolicyRule rule) {
 		if (rules==null) rules = new ArrayList<PolicyRule>();
 		rules.add(rule);
+		return rule;
 	}
 	/**
 	 * 
