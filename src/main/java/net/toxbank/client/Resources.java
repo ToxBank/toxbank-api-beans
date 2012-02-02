5e13c0cd382a37ba05a37d223787dfafb9a0c00f
ngn <jeliazkova.nina@gmail.com>
2012-02-02 14:17:50 +0200

    link for the previous version

--- src/main/java/net/toxbank/client/Resources.java
+++ src/main/java/net/toxbank/client/Resources.java
@@ -7,6 +7,7 @@ public class Resources {
 	public static final String organisation = "/organisation";
 	public static final String user = "/user";
 	public static final String versions = "/versions";
+	public static final String previous = "/previous";
 	public static final String authors = "/authors";
 	public static final String document = "/document";
 
